019ab0a98123d89dad29aab950750bded1b886b5
Rich Linnell <richlinnell@gmail.com>
2020-05-01 21:05:29 +0100

    Using a package reference

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"example.com/user/hello/morestrings"
+)
 
 func main(){
-	fmt.Println("Jees, this is rocket science!\n")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 }
